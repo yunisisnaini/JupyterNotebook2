921bb351a37af414a59d4729d91fa021f87a69aa
Marcellinus <68180695+IMarcellinus@users.noreply.github.com>
2020-10-21 14:30:27 +0700

    Create strukturkondisi.py

---
+++ strukturkondisi.py
@@ -0,0 +1,8 @@
+# STRUKTUR KONDISI
+# cek_umur.py
+umur = input("Berapa umur kamu: ")
+umur1 = input("Berapa umur maksimal boleh ambil SIM: ")
+if umur >= umur1:
+    print("Kamu boleh membuat SIM")
+else:
+    print("Kamu belum boleh membuat SIM")
